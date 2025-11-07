2dd2f87e11321ec988a9cf22b96335520c349f7a
Bruno <bruno.krown@istea.com.ar>
2025-11-07 13:32:20 -0300

    Elimino env.js de .gitignore

---
+++ env.js
@@ -0,0 +1,3 @@
+export const AIRTABLE_TOKEN = "patl7kTQUOmgS72Qx.aefe32b457017f298c0371f2f60f54405df87d845c107700362db3271a02e5b6";
+export const BASE_ID = "appH0zQj2QXP3U1fC";
+export const TABLE_NAME = "Productos";
