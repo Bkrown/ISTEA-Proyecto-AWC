69f0a9d4d032e8aa8f8646900ab35a71edb23268
Bruno <bruno.krown@istea.com.ar>
2025-11-11 15:16:16 -0300

    Agrego registro.html y method get en login.html

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ document.addEventListener("DOMContentLoaded", function() {
   // Events ------------------------------------------------
   
   
+  
   inputSearch.addEventListener('keyup', (event) => {
     // Filtro por nombre
     const text = event.target.value;
